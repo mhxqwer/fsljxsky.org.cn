6d18ea6bcf29a8c5a57cf2c1e30afd074a1434bf
mhxqwer <168000686+mhxqwer@users.noreply.github.com>
2025-12-02 23:38:43 +0800

    Delete static directory

--- static/js/advertising.js
+++
@@ -1,7 +0,0 @@
-document.write('<div class="screenfull">');
-document.write('<div class="screenfbtn"><a style="display: none" class="screenfreplay" title="重播" href="#"></a><a class=screenfclose title="关闭" href="#"></a></div>');
-document.write('<div class="screenfcon">');
-document.write('<img src="/UploadFiles/Document/202210/19182109187177.jpg" width="100%" height="100%" border="0">' );
-document.write('</div>');
-document.write('</div>');
-document.write('<script type="text/javascript" src="/Js/tools/fullscreen.js"></script>');
--- static/js/advertising2.js
+++
@@ -1,1 +0,0 @@
-﻿document.write('<img src="/UploadFiles/Document/201909/23093955633083.jpg" width=118 height=118 border=0 />');
